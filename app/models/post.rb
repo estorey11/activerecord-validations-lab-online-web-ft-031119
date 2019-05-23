8af38f95877bda18cc305b48b37470d2a024d520
Eric Storey <estorey@gmail.com>
2019-05-23 17:20:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,6 @@ class Post < ActiveRecord::Base
   def clickbaity?
     if title
       title.match(/Won't Believe/i) || title.match(/Secret/i) || title.match(/Top [0-9]*/i) || title.match(/Guess/i)
-      binding.pry 
-
     else 
       false
     end
