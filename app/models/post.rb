bea1ec10168e2eef1cd9afda9747bc0ec3822a9c
Eric Storey <estorey@gmail.com>
2019-05-23 17:16:19 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ActiveRecord::Base
   validate :clickbaity?
 
   def clickbaity?
-    @title.match(/Won't Believe/i) || @title.match(/Secret/i) || @title.match(/Top [0-9]*/i) || @title.match(/Guess/i)
+    if title && (title.match(/Won't Believe/i) || title.match(/Secret/i) || title.match(/Top [0-9]*/i) || title.match(/Guess/i))
   end
 end
