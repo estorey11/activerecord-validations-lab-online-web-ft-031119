2a71e82dabcfa08b2c1c668a312f46685ae3ebc3
Eric Storey <estorey@gmail.com>
2019-05-23 17:24:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ActiveRecord::Base
   validate :clickbaity?
 
   def clickbaity?
+    if title=="True Facts"
+      binding.pry
+    end
+    
     if title
       title.match(/Won't Believe/i) || title.match(/Secret/i) || title.match(/Top [0-9]*/i) || title.match(/Guess/i)
     else 
